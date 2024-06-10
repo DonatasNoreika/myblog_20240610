21db69c82bb587ce210c7ddd6ce74567cb5bbad1
DonatasNoreika <donoras@gmail.com>
2024-06-10 19:26:56 +0300

    +Galimybė palikti komentarą prisijungusiam vartotojui prie posto

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from django.views.generic import ListView, DetailView
 from .models import Post
+from django.views.generic.edit import FormMixin
+from .forms import CommentForm
 
 # Create your views here.
 class PostListView(ListView):
@@ -10,8 +12,28 @@ class PostListView(ListView):
     paginate_by = 5
 
 
-class PostDetailView(DetailView):
+class PostDetailView(FormMixin, DetailView):
     model = Post
     template_name = "post.html"
     context_object_name = "post"
+    form_class = CommentForm
+    # success_url = "/"
+
+    def get_success_url(self):
+        return reverse('post', kwargs={"pk": self.object.pk})
+
+    # standartinis post metodo perrašymas, naudojant FormMixin, galite kopijuoti tiesiai į savo projektą.
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.post = self.object
+        form.save()
+        return super().form_valid(form)
 
