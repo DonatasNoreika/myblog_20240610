025e5a8da1b91629f0e7ec39544208f6093252dc
DonatasNoreika <donoras@gmail.com>
2024-06-10 16:57:07 +0300

    +Admin aprašymas

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post, Comment
+
+
+class CommentInLine(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'content', 'date_created', 'author']
+    inlines = [CommentInLine]
+
 
 # Register your models here.
+admin.site.register(Post, PostAdmin)
